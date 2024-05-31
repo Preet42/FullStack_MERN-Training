82d17dc60e40caf7c3aad7338a02f42b0a40c361
Preet42 <147089874+Preet42@users.noreply.github.com>
2024-05-31 15:17:04 +0530

    Add files via upload

---
+++ Day_11/Component2/B.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import C from "./C";
+const B = () => {
+    return (
+        <div>
+            <h2>A Component</h2>
+            <C/>
+        </div>
+    )
+}
+export default B  
